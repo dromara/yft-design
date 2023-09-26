59930c38a6d698e944007ae0adf29b27213fb67f
Nevermore <15972699417@163.com>
2023-09-26 11:28:57 +0800

    新增元素裁切

--- src/views/Editor/CanvasRight/Components/ElementClip.vue
+++ src/views/Editor/CanvasRight/Components/ElementClip.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="element-text">
+  <div class="element-clip">
     <div class="row">
       <div style="flex: 2;"><b>启用剪切：</b></div>
       <div class="switch-wrapper" style="flex: 3;">
@@ -7,28 +7,47 @@
       </div>
     </div>
     <template v-if="hasClippath">
-      <el-row class="mt-10" >
-        <el-col :span="12">
-          <el-select>
-            <el-option value="0"></el-option>
-          </el-select>
-        </el-col>
-        <el-col :span="12">
-          <el-select>
-            <el-option value="0"></el-option>
-          </el-select>
-        </el-col>
-      </el-row> 
+      <div class="shape-box">
+        <div class="category" v-for="item in PathShapeLibs" :key="item.type">
+          <div class="shape-list">
+            <div class="shape-item" v-for="(shape, index) in item.children" :key="index">
+              <div class="shape-content" @click="selectShape(shape)">
+                <svg overflow="visible" width="20" height="20">
+                  <g :transform="`scale(${20 / shape.viewBox[0]}, ${20 / shape.viewBox[1]}) translate(0,0) matrix(1,0,0,1,0,0)`">
+                    <path 
+                      class="shape-path"
+                      :class="{ 'outlined': shape.outlined }"
+                      vector-effect="non-scaling-stroke" 
+                      stroke-linecap="butt" 
+                      stroke-miterlimit="8"
+                      :fill="shape.outlined ? '#999' : 'transparent'"
+                      :stroke="shape.outlined ? 'transparent' : '#999'"
+                      stroke-width="2" 
+                      :d="shape.path"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, ref, watch } from 'vue'
+import { PathShapeLibs } from '@/configs/shape'
+import { PathPoolItem } from '@/types/elements'
 const fontColor = ref('#000')
 const hasClippath = ref(true)
 
 const toggleStroke = () => {
+
+}
+
+const selectShape = (shape: PathPoolItem) => {
 
 }
 </script>
@@ -43,7 +62,50 @@ const toggleStroke = () => {
 .switch-wrapper {
   text-align: right;
 }
-.slider {
-  flex: 3;
+.shape-pool {
+  width: 100%;
+  margin-top: -12px;
+  margin-bottom: -12px;
+  margin-right: -12px;
+  padding-right: 12px;
+  padding-top: 12px;
+}
+.shape-box {
+  border: 1px solid $borderColor;
+  border-radius: $borderRadius;
+  max-height: 200px;
+  overflow: auto
+}
+.shape-list {
+  @include flex-grid-layout();
+  margin-bottom: 10px;
+}
+.shape-item {
+  @include flex-grid-layout-children(6, 16%);
+  height: 0;
+  padding-bottom: 16%;
+  flex-shrink: 0;
+  position: relative;
+  cursor: pointer;
+}
+.shape-content {
+  @include absolute-0();
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover .shape-path {
+    &:not(.outlined) {
+      stroke: $themeColor;
+    }
+    &.outlined {
+      fill: $themeColor;
+    }
+  }
+
+  svg:not(:root) {
+    overflow: visible;
+  }
 }
 </style>
