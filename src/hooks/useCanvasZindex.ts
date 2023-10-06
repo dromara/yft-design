8471f526a20ddece4bbffa302f3ec1c8f6f1c1f9
Nevermore <15972699417@163.com>
2023-10-06 16:47:57 +0800

    更新设置元素层级

--- src/hooks/useCanvasZindex.ts
+++ src/hooks/useCanvasZindex.ts
@@ -3,17 +3,16 @@ import {
   WorkSpaceDrawType, 
   WorkSpaceMaskType, 
   WorkSpaceSafeType, 
-  WorkSpaceLineType 
 } from "@/configs/canvas"
+import { CanvasElement } from "@/types/canvas"
 import { Canvas } from "fabric/fabric-impl"
 
 export default () => {
   const setZindex = (canvas: Canvas) => {
-    canvas.sendObjectToBack(canvas.getObjects(WorkSpaceDrawType)[0])
-    canvas.bringObjectToFront(canvas.getObjects(WorkSpaceMaskType)[0])
-    canvas.bringObjectToFront(canvas.getObjects(WorkSpaceClipType)[0])
-    canvas.bringObjectToFront(canvas.getObjects(WorkSpaceSafeType)[0])
-    canvas.bringObjectToFront(canvas.getObjects(WorkSpaceLineType)[0])
+    canvas.sendObjectToBack(canvas.getObjects().filter(ele => (ele as CanvasElement).id === WorkSpaceDrawType)[0])
+    canvas.bringObjectToFront(canvas.getObjects().filter(ele => (ele as CanvasElement).id === WorkSpaceMaskType)[0])
+    canvas.bringObjectToFront(canvas.getObjects().filter(ele => (ele as CanvasElement).id === WorkSpaceClipType)[0])
+    canvas.bringObjectToFront(canvas.getObjects().filter(ele => (ele as CanvasElement).id === WorkSpaceSafeType)[0])
   }
   return {
     setZindex
