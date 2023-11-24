a07890b26c010f0aa188dac2c78097fbf702d1f4
黄某人 <loveme7701@outlook.com>
2023-11-24 10:12:58 +0800

    🐞 fix: 修复 shift 移动画布 bug

--- src/app/wheelScroll.ts
+++ src/app/wheelScroll.ts
@@ -28,7 +28,7 @@ export class WheelScroll extends Disposable {
     const mouseWheel = (e: CanvasEvents['mouse:wheel']) => {
       e.e.preventDefault()
       e.e.stopPropagation()
-      const { deltaY, offsetX, offsetY } = e.e
+      const { deltaX ,deltaY, offsetX, offsetY } = e.e
       // 缩放视窗
       if (ctrl.value || cmd.value) {
         const zoomFactor = Math.abs(deltaY) < 10 ? deltaY * 2 : deltaY / 3
@@ -46,7 +46,7 @@ export class WheelScroll extends Disposable {
       // 滚动画布
       const deltaPoint = new Point()
       if (shift.value) {
-        deltaPoint.x = deltaY > 0 ? -20 : 20
+        deltaPoint.x = deltaX > 0 ? -20 : 20
       } else {
         deltaPoint.y = deltaY > 0 ? -20 : 20
       }
