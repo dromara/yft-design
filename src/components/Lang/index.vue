424dbf96192a9a1d64c41cf21f2f0fe899554f79
Nevermore <15972699417@163.com>
2024-03-08 15:50:11 +0800

    更新切换语言生效

--- src/components/Lang/index.vue
+++ src/components/Lang/index.vue
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <el-dropdown trigger="click">
+  <el-dropdown trigger="click" @command="onDropdown">
     <span class="handler-dropdown">
       <IconTranslate class="handler-icon"/>
     </span>
