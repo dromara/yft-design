aaf8abdc9710a6677463d4b5227203db7b17911d
wohuweixi <1912715645@qq.com>
2024-03-14 19:54:22 +0800

    fix: IllustrationCategory和ImageCategory组件的for循环添加key

--- src/views/Editor/CanvasMenu/components/ImageComponents/IllustrationCategory.vue
+++ src/views/Editor/CanvasMenu/components/ImageComponents/IllustrationCategory.vue
@@ -1,78 +1,139 @@
 <template>
   <div>
-    <div class="category-container" ref="categoryRef" @scroll="onCategoryScroll" v-if="typeRef === 'all'">
-      <div v-for="item in illustrationCategoryData">
-        <el-row class="col-tip mt-5" >
+    <div
+      class="category-container"
+      ref="categoryRef"
+      @scroll="onCategoryScroll"
+      v-if="typeRef === 'all'"
+    >
+      <div v-for="(item, index) in illustrationCategoryData" :key="index">
+        <el-row class="col-tip mt-5">
           <el-col :span="5" class="col-name">
             <el-tag>{{ item.name }}</el-tag>
           </el-col>
           <el-col :span="7" class="col-name">
-            <el-button text @click="showTotal(item.type)">{{$t('message.all')}}<IconRight/></el-button>
+            <el-button text @click="showTotal(item.type)"
+              >{{ $t("message.all") }}<IconRight
+            /></el-button>
           </el-col>
         </el-row>
-        <el-row class="category-box mt-5" v-loading="item.category.length === 0">
-          <div class="box-image" :style="{width: (img.previewHeight <= 120 ? img.previewWidth / img.previewHeight * 120 : img.previewWidth) + 'px'}" v-for="img in item.category">
-            <img :src="img.previewURL" :alt="img.tags" @click="createImage(img)">
+        <el-row
+          class="category-box mt-5"
+          v-loading="item.category.length === 0"
+        >
+          <div
+            class="box-image"
+            :style="{
+              width:
+                (img.previewHeight <= 120
+                  ? (img.previewWidth / img.previewHeight) * 120
+                  : img.previewWidth) + 'px',
+            }"
+            v-for="(img, index) in item.category"
+            :key="index"
+          >
+            <img
+              :src="img.previewURL"
+              :alt="img.tags"
+              @click="createImage(img)"
+            />
           </div>
         </el-row>
       </div>
     </div>
-    <div class="category-container" ref="totalRef" @scroll="onTotalScroll" v-else>
+    <div
+      class="category-container"
+      ref="totalRef"
+      @scroll="onTotalScroll"
+      v-else
+    >
       <el-row class="col-tip mt-5">
         <el-col :span="7" class="col-name">
-          <el-button text @click="hideTotal()"><IconLeft/>{{ categoryData.name }}</el-button>
+          <el-button text @click="hideTotal()"
+            ><IconLeft />{{ categoryData.name }}</el-button
+          >
         </el-col>
       </el-row>
-      <el-row class="total-box mt-5" v-loading="categoryData.total.length === 0">
-        <div class="box-image" v-for="(img, index) in categoryData.total" :style="{justifyContent: index % 2 === 0 ? 'flex-start' : 'flex-end'}">
-          <img :src="img.previewURL" :alt="img.tags" @click="createImage(img)">
+      <el-row
+        class="total-box mt-5"
+        v-loading="categoryData.total.length === 0"
+      >
+        <div
+          class="box-image"
+          v-for="(img, index) in categoryData.total"
+          :key="index"
+          :style="{
+            justifyContent: index % 2 === 0 ? 'flex-start' : 'flex-end',
+          }"
+        >
+          <img
+            :src="img.previewURL"
+            :alt="img.tags"
+            @click="createImage(img)"
+          />
         </div>
       </el-row>
     </div>
-    <el-row class="image-bottom">{{$t('message.all')}}</el-row>
+    <el-row class="image-bottom">{{ $t("message.all") }}</el-row>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, computed } from 'vue'
-import { debounce, throttle } from 'lodash'
-import { getIllustrationPages, getIllustrationCategory } from '@/api/image'
-import { ImageHit } from '@/api/image/types'
-import { useMainStore } from '@/store'
-import { storeToRefs } from 'pinia'
-import useHandleCreate from '@/hooks/useHandleCreate'
-const mainStore = useMainStore()
-const { illustrationCategoryType, illustrationCategoryData } = storeToRefs(mainStore)
-const { createImageElement } = useHandleCreate()
+import { onMounted, ref, computed } from "vue";
+import { debounce, throttle } from "lodash";
+import { getIllustrationPages, getIllustrationCategory } from "@/api/image";
+import { ImageHit } from "@/api/image/types";
+import { useMainStore } from "@/store";
+import { storeToRefs } from "pinia";
+import useHandleCreate from "@/hooks/useHandleCreate";
+const mainStore = useMainStore();
+const { illustrationCategoryType, illustrationCategoryData } =
+  storeToRefs(mainStore);
+const { createImageElement } = useHandleCreate();
 
 const categoryRef = ref<HTMLDivElement>();
 const totalRef = ref<HTMLDivElement>();
-const categoryTop = ref(0)
-const typeRef = ref('all')
+const categoryTop = ref(0);
+const typeRef = ref("all");
 const categoryData = computed(() => {
-  return illustrationCategoryData.value.filter(ele => ele.type === typeRef.value)[0]
-})
-const getImageCategoryData = throttle( async (t: string) => {
-  const res = await getIllustrationCategory({t})
-  if (res && res.data.code === 200) {
-    illustrationCategoryData.value.filter(item => item.type === t).map(ele => ele.category = res.data.data.slice(0, 2))
-  }
-}, 100, { leading: true, trailing: false  })
+  return illustrationCategoryData.value.filter(
+    (ele) => ele.type === typeRef.value
+  )[0];
+});
+const getImageCategoryData = throttle(
+  async (t: string) => {
+    const res = await getIllustrationCategory({ t });
+    if (res && res.data.code === 200) {
+      illustrationCategoryData.value
+        .filter((item) => item.type === t)
+        .map((ele) => (ele.category = res.data.data.slice(0, 2)));
+    }
+  },
+  100,
+  { leading: true, trailing: false }
+);
 
-const getImagePageData = throttle( async (t: string, page: 1) => {
-  const res = await getIllustrationPages({t, page})
-  if (res && res.data.code === 200) {
-    illustrationCategoryData.value.filter(item => item.type === t).map(ele => ele.total = res.data.data)
-  }
-}, 100, { leading: true, trailing: false })
+const getImagePageData = throttle(
+  async (t: string, page: 1) => {
+    const res = await getIllustrationPages({ t, page });
+    if (res && res.data.code === 200) {
+      illustrationCategoryData.value
+        .filter((item) => item.type === t)
+        .map((ele) => (ele.total = res.data.data));
+    }
+  },
+  100,
+  { leading: true, trailing: false }
+);
 
 const getContainScroll = () => {
-  let startIndex = 0, endIndex = 2
-  if (!categoryRef.value) 
-  return {
-    startIndex,
-    endIndex
-  }
+  let startIndex = 0,
+    endIndex = 2;
+  if (!categoryRef.value)
+    return {
+      startIndex,
+      endIndex,
+    };
   const scrollTop = categoryRef.value.scrollTop;
   const containerHeight = categoryRef.value.clientHeight;
   const itemHeight = 132;
@@ -80,62 +141,61 @@ const getContainScroll = () => {
   endIndex = Math.ceil((scrollTop + containerHeight) / itemHeight);
   return {
     startIndex,
-    endIndex
-  }
-}
+    endIndex,
+  };
+};
 
 const onCategoryScroll = async () => {
-  const { startIndex, endIndex } = getContainScroll()
+  const { startIndex, endIndex } = getContainScroll();
   for (let i = startIndex; i < endIndex; i++) {
-    const item = illustrationCategoryData.value[i]
-    if (!item) return
+    const item = illustrationCategoryData.value[i];
+    if (!item) return;
     if (!illustrationCategoryType.value.includes(item.type)) {
-      illustrationCategoryType.value.push(item.type)
-      await getImageCategoryData(item.type)
+      illustrationCategoryType.value.push(item.type);
+      await getImageCategoryData(item.type);
     }
     // if (item.category.length === 0) {
     //   await getImageCategoryData(item.type)
     // }
   }
-}
+};
 
 const onTotalScroll = async () => {
-  const { startIndex, endIndex } = getContainScroll()
+  const { startIndex, endIndex } = getContainScroll();
   for (let i = startIndex; i < endIndex; i++) {
-    const item = illustrationCategoryData.value[i]
-    if (!item) return
+    const item = illustrationCategoryData.value[i];
+    if (!item) return;
     if (!illustrationCategoryType.value.includes(item.type)) {
-      illustrationCategoryType.value.push(item.type)
-      await getImageCategoryData(item.type)
+      illustrationCategoryType.value.push(item.type);
+      await getImageCategoryData(item.type);
     }
     // if (item.category.length === 0) {
     //   await getImageCategoryData(item.type)
     // }
   }
-}
+};
 
 const showTotal = async (type: string) => {
-  if (!categoryRef.value) return
-  categoryTop.value = categoryRef.value.scrollTop
-  typeRef.value = type
-  await getImagePageData(type, 1)
-}
+  if (!categoryRef.value) return;
+  categoryTop.value = categoryRef.value.scrollTop;
+  typeRef.value = type;
+  await getImagePageData(type, 1);
+};
 
 const hideTotal = () => {
-  typeRef.value = 'all'
-  if (!categoryRef.value) return
-  categoryRef.value.scrollTo({top: categoryTop.value, behavior: 'smooth'})
-}
+  typeRef.value = "all";
+  if (!categoryRef.value) return;
+  categoryRef.value.scrollTo({ top: categoryTop.value, behavior: "smooth" });
+};
 
 const createImage = (item: ImageHit) => {
-  createImageElement(item.largeImageURL)
-}
+  createImageElement(item.largeImageURL);
+};
 
 onMounted(() => {
-  if (!categoryRef.value) return
-  onCategoryScroll()
-})
-
+  if (!categoryRef.value) return;
+  onCategoryScroll();
+});
 </script>
 
 <style lang="scss" scoped>
