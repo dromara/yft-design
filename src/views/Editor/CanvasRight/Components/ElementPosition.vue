12f7ae0cf2436ab907f50a42fb296fc9316d7b5c
wohuweixi <1912715645@qq.com>
2024-03-22 23:04:20 +0800

    feat:翻译条码组件样式

--- src/views/Editor/CanvasRight/Components/ElementPosition.vue
+++ src/views/Editor/CanvasRight/Components/ElementPosition.vue
@@ -2,34 +2,82 @@
   <div class="element-position">
     <div class="mb-10">
       <el-row>
-        <el-tooltip placement="top" :hide-after="0" content="左对齐">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.LEFT)">
-            <IconAlignLeft/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.leftAlign')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.LEFT)"
+          >
+            <IconAlignLeft />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="水平居中">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.HORIZONTAL)">
-            <IconAlignVertically/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.centerHorizontal')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.HORIZONTAL)"
+          >
+            <IconAlignVertically />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="右对齐">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.RIGHT)">
-            <IconAlignRight/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.rightAlign')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.RIGHT)"
+          >
+            <IconAlignRight />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="上对齐">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.TOP)">
-            <IconAlignTop/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.topAlign')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.TOP)"
+          >
+            <IconAlignTop />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="垂直居中">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.VERTICAL)">
-            <IconAlignHorizontally/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.verticalCenter')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.VERTICAL)"
+          >
+            <IconAlignHorizontally />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="下对齐">
-          <el-col :span="4" class="align-item" @click="alignElement(AlignCommand.BOTTOM)">
-            <IconAlignBottom/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.bottomAlign')"
+        >
+          <el-col
+            :span="4"
+            class="align-item"
+            @click="alignElement(AlignCommand.BOTTOM)"
+          >
+            <IconAlignBottom />
           </el-col>
         </el-tooltip>
       </el-row>
@@ -37,79 +85,116 @@
     <div class="mb-10">
       <el-row>
         <el-col :span="11" class="position-col">
-          <SwipeInput v-bind="left" content='X'/>
+          <SwipeInput v-bind="left" content="X" />
         </el-col>
         <el-col :span="2" class="fixed-ratio">
-          <el-tooltip effect="dark"  placement="top" content="解除宽高比" v-if="isFixed">
-            <IconLock class="icon-btn" @click="changeFixedRatio(false)"/>
+          <el-tooltip
+            effect="dark"
+            placement="top"
+            :content="$t('style.unlockAspectRatio')"
+            v-if="isFixed"
+          >
+            <IconLock class="icon-btn" @click="changeFixedRatio(false)" />
           </el-tooltip>
-          <el-tooltip effect="dark" placement="top" content="锁定宽高比" v-else>
-            <IconUnlock class="icon-btn" @click="changeFixedRatio(true)"/>
+          <el-tooltip
+            effect="dark"
+            placement="top"
+            :content="$t('style.lockAspectRatio')"
+            v-else
+          >
+            <IconUnlock class="icon-btn" @click="changeFixedRatio(true)" />
           </el-tooltip>
         </el-col>
         <el-col :span="11" class="position-col">
-          <SwipeInput v-bind="top" content='Y'/>
+          <SwipeInput v-bind="top" content="Y" />
         </el-col>
       </el-row>
     </div>
     <div class="mb-10">
       <el-row>
         <el-col :span="11" class="position-col">
-          <SwipeInput v-bind="width" content='W'/>
+          <SwipeInput v-bind="width" content="W" />
         </el-col>
         <el-col :span="2" class="fixed-ratio">
-          <el-tooltip effect="dark"  placement="top" content="解除宽高比" v-if="isFixed">
-            <IconLock class="icon-btn" @click="changeFixedRatio(false)"/>
+          <el-tooltip
+            effect="dark"
+            placement="top"
+            :content="$t('style.unlockAspectRatio')"
+            v-if="isFixed"
+          >
+            <IconLock class="icon-btn" @click="changeFixedRatio(false)" />
           </el-tooltip>
-          <el-tooltip effect="dark" placement="top" content="锁定宽高比" v-else>
-            <IconUnlock class="icon-btn" @click="changeFixedRatio(true)"/>
+          <el-tooltip
+            effect="dark"
+            placement="top"
+            :content="$t('style.lockAspectRatio')"
+            v-else
+          >
+            <IconUnlock class="icon-btn" @click="changeFixedRatio(true)" />
           </el-tooltip>
         </el-col>
         <el-col :span="11" class="position-col">
-          <SwipeInput v-bind="height" content='H'/>
+          <SwipeInput v-bind="height" content="H" />
         </el-col>
       </el-row>
     </div>
     <div class="mb-10">
       <el-row>
         <el-col :span="11" class="position-col">
-          <SwipeInput v-bind="angle" content='A'/>
+          <SwipeInput v-bind="angle" content="A" />
         </el-col>
-        <el-col :span="2" class="fixed-ratio">
-        </el-col>
+        <el-col :span="2" class="fixed-ratio"> </el-col>
         <el-col :span="5" class="angle-col">
-          <div @click="changeRotate45('-')">
-            <IconRotate/> -45°
-          </div>
+          <div @click="changeRotate45('-')"><IconRotate /> -45°</div>
         </el-col>
         <el-col :span="1"></el-col>
         <el-col :span="5" class="angle-col">
           <div @click="changeRotate45('+')">
-            <IconRotate :style="{ transform: 'rotateY(180deg)' }"/> +45°
+            <IconRotate :style="{ transform: 'rotateY(180deg)' }" /> +45°
           </div>
         </el-col>
       </el-row>
     </div>
     <div>
       <el-row>
-        <el-tooltip placement="top" :hide-after="0" content="置顶">
-          <el-col :span="6" class="align-item" @click="layerElement(LayerCommand.TOP)">
-            <IconSendToBack/>
+        <el-tooltip placement="top" :hide-after="0" :content="$t('style.top')">
+          <el-col
+            :span="6"
+            class="align-item"
+            @click="layerElement(LayerCommand.TOP)"
+          >
+            <IconSendToBack />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="置底">
-          <el-col :span="6" class="align-item" @click="layerElement(LayerCommand.BOTTOM)">
-            <IconBringToFrontOne/>
+        <el-tooltip
+          placement="top"
+          :hide-after="0"
+          :content="$t('style.bottom')"
+        >
+          <el-col
+            :span="6"
+            class="align-item"
+            @click="layerElement(LayerCommand.BOTTOM)"
+          >
+            <IconBringToFrontOne />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="下移">
-          <el-col :span="6" class="align-item" @click="layerElement(LayerCommand.DOWN)">
-            <IconSendBackward/>
+        <el-tooltip placement="top" :hide-after="0" :content="$t('style.down')">
+          <el-col
+            :span="6"
+            class="align-item"
+            @click="layerElement(LayerCommand.DOWN)"
+          >
+            <IconSendBackward />
           </el-col>
         </el-tooltip>
-        <el-tooltip placement="top" :hide-after="0" content="上移">
-          <el-col :span="6" class="align-item" @click="layerElement(LayerCommand.UP)">
-            <IconBringForward/>
+        <el-tooltip placement="top" :hide-after="0" :content="$t('style.up')">
+          <el-col
+            :span="6"
+            class="align-item"
+            @click="layerElement(LayerCommand.UP)"
+          >
+            <IconBringForward />
           </el-col>
         </el-tooltip>
       </el-row>
@@ -118,48 +203,45 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { storeToRefs } from 'pinia'
-import { AlignCommand, LayerCommand } from '@/types/elements'
-import { useMainStore, useTemplatesStore } from '@/store'
-import useCanvas from '@/views/Canvas/useCanvas'
-import useHandleTool from '@/hooks/useHandleTool'
-import useHandleActive from '@/hooks/useHandleActive'
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { AlignCommand, LayerCommand } from "@/types/elements";
+import { useMainStore, useTemplatesStore } from "@/store";
+import useCanvas from "@/views/Canvas/useCanvas";
+import useHandleTool from "@/hooks/useHandleTool";
+import useHandleActive from "@/hooks/useHandleActive";
 
-const templatesStore = useTemplatesStore()
-const { alignElement, layerElement } = useHandleTool()
-const [ canvas ] = useCanvas()
-const { canvasObject } = storeToRefs(useMainStore())
-const { handleActive } = useHandleActive()
-const left = handleActive('left')
-const top = handleActive('top')
-const height = handleActive('height')
-const width = handleActive('width')
-const angle = handleActive('angle')
+const templatesStore = useTemplatesStore();
+const { alignElement, layerElement } = useHandleTool();
+const [canvas] = useCanvas();
+const { canvasObject } = storeToRefs(useMainStore());
+const { handleActive } = useHandleActive();
+const left = handleActive("left");
+const top = handleActive("top");
+const height = handleActive("height");
+const width = handleActive("width");
+const angle = handleActive("angle");
 // const handleElement = computed(() => canvasObject.value as CanvasElement)
 // const { x, y } = getWidthHeight(canvas.activeObject.value as FabricObject)
-const isFixed = ref(false)
-
+const isFixed = ref(false);
 
 const changeFixedRatio = (status: boolean) => {
-  isFixed.value = status
-}
+  isFixed.value = status;
+};
 
 // 修改旋转45度（顺时针或逆时针）
-const changeRotate45 = (command: '+' | '-') => {
-  const [ canvas ] = useCanvas()
-  if (!canvasObject.value || !canvas) return
-  let _rotate = Math.floor(canvasObject.value.angle / 45) * 45
-  if (command === '+') _rotate = _rotate + 45
-  else if (command === '-') _rotate = _rotate - 45
-  if (_rotate < -180) _rotate = -180
-  if (_rotate > 180) _rotate = 180
-  canvasObject.value.angle = _rotate
-  canvas.renderAll()
-  templatesStore.modifedElement()
-}
-
-
+const changeRotate45 = (command: "+" | "-") => {
+  const [canvas] = useCanvas();
+  if (!canvasObject.value || !canvas) return;
+  let _rotate = Math.floor(canvasObject.value.angle / 45) * 45;
+  if (command === "+") _rotate = _rotate + 45;
+  else if (command === "-") _rotate = _rotate - 45;
+  if (_rotate < -180) _rotate = -180;
+  if (_rotate > 180) _rotate = 180;
+  canvasObject.value.angle = _rotate;
+  canvas.renderAll();
+  templatesStore.modifedElement();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -170,7 +252,7 @@ const changeRotate45 = (command: '+' | '-') => {
   align-items: center;
   cursor: pointer;
   border-radius: $borderRadius;
-  &:hover{
+  &:hover {
     background-color: #f1f1f1;
   }
 }
@@ -184,7 +266,7 @@ const changeRotate45 = (command: '+' | '-') => {
 }
 .position-col {
   :deep(.el-input-number) {
-    width: auto
+    width: auto;
   }
 }
 .position-col {
@@ -209,7 +291,7 @@ const changeRotate45 = (command: '+' | '-') => {
   align-items: center;
   cursor: pointer;
   border-radius: $borderRadius;
-  &:hover{
+  &:hover {
     background-color: #f1f1f1;
   }
 }
--- src/views/Editor/CanvasRight/Components/ElementShadow.vue
+++ src/views/Editor/CanvasRight/Components/ElementShadow.vue
@@ -1,45 +1,79 @@
 <template>
   <div class="element-shadow">
     <div class="row">
-      <div style="flex: 2;"><b>启用阴影：</b></div>
-      <div class="switch-wrapper" style="flex: 3;">
-        <el-switch v-model="props.hasShadow" @change="toggleShadow()"></el-switch>
+      <div style="flex: 2">
+        <b>{{ $t("style.enableShadow") }}：</b>
+      </div>
+      <div class="switch-wrapper" style="flex: 3">
+        <el-switch
+          v-model="props.hasShadow"
+          @change="toggleShadow()"
+        ></el-switch>
       </div>
     </div>
     <template v-if="props.hasShadow">
       <el-row>
-        <el-col :span="7" class="slider-name">水平阴影：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.horizontalShadow") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="10">
-          <el-slider class="slider" v-model="offsetX" :min="1" :max="10" @change="changeOffsetX"></el-slider>
+          <el-slider
+            class="slider"
+            v-model="offsetX"
+            :min="1"
+            :max="10"
+            @change="changeOffsetX"
+          ></el-slider>
         </el-col>
         <el-col :span="4" class="slider-num">{{ offsetX }}</el-col>
       </el-row>
       <el-row>
-        <el-col :span="7" class="slider-name">垂直阴影：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.verticalShadow") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="10">
-          <el-slider class="slider" v-model="offsetY" :min="1" :max="10" @change="changeOffsetY"></el-slider>
+          <el-slider
+            class="slider"
+            v-model="offsetY"
+            :min="1"
+            :max="10"
+            @change="changeOffsetY"
+          ></el-slider>
         </el-col>
         <el-col :span="4" class="slider-num">{{ offsetY }}</el-col>
       </el-row>
       <el-row>
-        <el-col :span="7" class="slider-name">模糊距离：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.fuzzyDistance") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="10">
-          <el-slider class="slider" v-model="blur" :min="1" :max="10" @change="changeBlur"></el-slider>
+          <el-slider
+            class="slider"
+            v-model="blur"
+            :min="1"
+            :max="10"
+            @change="changeBlur"
+          ></el-slider>
         </el-col>
         <el-col :span="4" class="slider-num">{{ blur }}</el-col>
       </el-row>
       <el-row>
-        <el-col :span="7" class="slider-name">阴影颜色：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.shadowColor") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="14">
           <el-popover trigger="click" width="265">
             <template #reference>
-              <ColorButton :color="shadowColor" style="flex: 3;" />
+              <ColorButton :color="shadowColor" style="flex: 3" />
             </template>
-            <ColorPicker :modelValue="shadowColor" @update:modelValue="(color: string) => updateShadowColor(color)"/>
+            <ColorPicker
+              :modelValue="shadowColor"
+              @update:modelValue="(color: string) => updateShadowColor(color)"
+            />
           </el-popover>
         </el-col>
       </el-row>
@@ -48,29 +82,29 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useMainStore } from '@/store'
-import * as fabric from 'fabric'
-import useCanvas from '@/views/Canvas/useCanvas'
-import { CanvasElement } from '@/types/canvas'
+import { computed, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useMainStore } from "@/store";
+import * as fabric from "fabric";
+import useCanvas from "@/views/Canvas/useCanvas";
+import { CanvasElement } from "@/types/canvas";
 
 const props = defineProps({
   hasShadow: {
     type: Boolean,
     required: true,
   },
-})
+});
 
-const [ canvas ] = useCanvas()
-const { canvasObject } = storeToRefs(useMainStore())
+const [canvas] = useCanvas();
+const { canvasObject } = storeToRefs(useMainStore());
 
-const offsetX = ref<number | undefined>(1)
-const offsetY = ref<number | undefined>(1)
-const blur = ref<number | undefined>(5)
-const shadowColor = ref('#000000')
+const offsetX = ref<number | undefined>(1);
+const offsetY = ref<number | undefined>(1);
+const blur = ref<number | undefined>(5);
+const shadowColor = ref("#000000");
 
-const handleElement = computed(() => canvasObject.value as CanvasElement)
+const handleElement = computed(() => canvasObject.value as CanvasElement);
 // const hasShadow = computed(() => {
 //   if (!handleElement.value) return false
 //   if (handleElement.value.shadow) {
@@ -82,48 +116,46 @@ const handleElement = computed(() => canvasObject.value as CanvasElement)
 //   }
 //   return false
 // })
-const openShadow = ref(props.hasShadow)
-
+const openShadow = ref(props.hasShadow);
 
 const updateShadowColor = (color: string) => {
-  shadowColor.value = color
-  updateShadowElement()
-}
+  shadowColor.value = color;
+  updateShadowElement();
+};
 
 const changeOffsetX = () => {
-  updateShadowElement()
-}
+  updateShadowElement();
+};
 
 const changeOffsetY = () => {
-  updateShadowElement()
-}
+  updateShadowElement();
+};
 
 const changeBlur = () => {
-  updateShadowElement()
-}
+  updateShadowElement();
+};
 
 const updateShadowElement = () => {
-  const [ canvas ] = useCanvas()
-  if (!handleElement.value) return
+  const [canvas] = useCanvas();
+  if (!handleElement.value) return;
   handleElement.value.shadow = new fabric.Shadow({
     color: shadowColor.value,
     offsetX: offsetX.value,
     offsetY: offsetY.value,
-    blur: blur.value
-  })
-  canvas.renderAll()
-}
+    blur: blur.value,
+  });
+  canvas.renderAll();
+};
 
 const toggleShadow = () => {
-  if (!handleElement.value) return
+  if (!handleElement.value) return;
   if (openShadow.value) {
-    updateShadowElement()
+    updateShadowElement();
+  } else {
+    handleElement.value.shadow = null;
   }
-  else {
-    handleElement.value.shadow = null
-  }
-  canvas.renderAll()
-}
+  canvas.renderAll();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -143,7 +175,7 @@ const toggleShadow = () => {
   display: flex;
   align-items: center;
 }
-.slider-num{
+.slider-num {
   display: flex;
   align-items: center;
   justify-content: center;
