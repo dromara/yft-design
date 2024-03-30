5f4be77627d1584c2475cfaa063185b8a95970ec
wohuweixiya <86701050+wohuweixiya@users.noreply.github.com>
2024-03-30 23:59:04 +0800

    fix: 修复ElementShadow组件启用阴影功能报错问题 (#61)

--- src/views/Editor/CanvasRight/Components/ElementMask.vue
+++ src/views/Editor/CanvasRight/Components/ElementMask.vue
@@ -1,29 +1,48 @@
 <template>
   <div class="element-color-mask">
     <div class="row">
-      <div style="flex: 1;"><b>启用蒙版：</b></div>
-      <div class="switch-wrapper" style="flex: 1;">
-        <el-switch v-model="openColorMask" @change="toggleColorMask"></el-switch>
+      <div style="flex: 1">
+        <b>{{ $t("style.enableMask") }}：</b>
+      </div>
+      <div class="switch-wrapper" style="flex: 1">
+        <el-switch
+          v-model="openColorMask"
+          @change="toggleColorMask"
+        ></el-switch>
       </div>
     </div>
     <template v-if="openColorMask">
       <el-row>
-        <el-col :span="7" class="slider-name">蒙版颜色：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.maskColor") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="14">
           <el-popover trigger="click" width="265">
             <template #reference>
-              <ColorButton :color="maskColor" style="flex: 3;" />
+              <ColorButton :color="maskColor" style="flex: 3" />
             </template>
-            <ColorPicker :modelValue="maskColor" @update:modelValue="(color: string) => updateMaskColor(color)"/>
+            <ColorPicker
+              :modelValue="maskColor"
+              @update:modelValue="(color: string) => updateMaskColor(color)"
+            />
           </el-popover>
         </el-col>
       </el-row>
       <el-row>
-        <el-col :span="7" class="slider-name">不透明度：</el-col>
+        <el-col :span="7" class="slider-name"
+          >{{ $t("style.opacity") }}：</el-col
+        >
         <el-col :span="3"></el-col>
         <el-col :span="10">
-          <el-slider class="slider" v-model="maskAlpha" :min="0" :max="1" :step="0.01" @change="updateMaskAlpha"></el-slider>
+          <el-slider
+            class="slider"
+            v-model="maskAlpha"
+            :min="0"
+            :max="1"
+            :step="0.01"
+            @change="updateMaskAlpha"
+          ></el-slider>
         </el-col>
         <el-col :span="4" class="slider-num">{{ maskAlpha }}</el-col>
       </el-row>
@@ -32,77 +51,83 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useMainStore } from '@/store'
-import { ImageElement } from '@/types/canvas'
-import { ElementNames } from '@/types/elements'
-import { filters, Image } from 'fabric'
-import useCanvas from '@/views/Canvas/useCanvas'
+import { computed, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useMainStore } from "@/store";
+import { ImageElement } from "@/types/canvas";
+import { ElementNames } from "@/types/elements";
+import { filters, Image } from "fabric";
+import useCanvas from "@/views/Canvas/useCanvas";
 
-const BlendColorFilter = 'BlendColor'
-const maskColor = ref('')
-const maskAlpha = ref(0.3)
-const [ canvas ] = useCanvas()
-const { canvasObject } = storeToRefs(useMainStore())
+const BlendColorFilter = "BlendColor";
+const maskColor = ref("");
+const maskAlpha = ref(0.3);
+const [canvas] = useCanvas();
+const { canvasObject } = storeToRefs(useMainStore());
 
-const handleElement = computed(() => canvasObject.value as Image)
+const handleElement = computed(() => canvasObject.value as Image);
 const hasColorMask = computed(() => {
-  if (!handleElement.value || handleElement.value.type !== ElementNames.IMAGE) return false
-  const blendColorFilter = handleElement.value.filters.filter(obj => obj.type === BlendColorFilter)[0] as filters.BlendColor
+  if (!handleElement.value || handleElement.value.type !== ElementNames.IMAGE)
+    return false;
+  const blendColorFilter = handleElement.value.filters.filter(
+    (obj) => obj.type === BlendColorFilter
+  )[0] as filters.BlendColor;
   if (blendColorFilter) {
-    maskColor.value = blendColorFilter.color
-    maskAlpha.value = blendColorFilter.alpha
-    return true
+    maskColor.value = blendColorFilter.color;
+    maskAlpha.value = blendColorFilter.alpha;
+    return true;
   }
-  return false
-})
-const openColorMask = ref(hasColorMask.value)
+  return false;
+});
+const openColorMask = ref(hasColorMask.value);
 
 const updateMaskColor = (color: string) => {
-  maskColor.value = color
-  changeImageFilter()
-}
+  maskColor.value = color;
+  changeImageFilter();
+};
 
 const updateMaskAlpha = () => {
-  changeImageFilter()
-}
+  changeImageFilter();
+};
 
 const changeImageFilter = () => {
   const blendFilter = new filters.BlendColor({
     color: maskColor.value,
-    mode: 'add',
-    alpha: maskAlpha.value
-  })
-  handleElement.value.filters = handleElement.value.filters.filter(obj => obj.type !== BlendColorFilter)
-  handleElement.value.filters.push(blendFilter as filters.BaseFilter)
-  handleElement.value.applyFilters()
-  canvas.renderAll()
-}
+    mode: "add",
+    alpha: maskAlpha.value,
+  });
+  handleElement.value.filters = handleElement.value.filters.filter(
+    (obj) => obj.type !== BlendColorFilter
+  );
+  handleElement.value.filters.push(blendFilter as filters.BaseFilter);
+  handleElement.value.applyFilters();
+  canvas.renderAll();
+};
 
 const toggleColorMask = () => {
-  if (!handleElement.value) return
-  const [ canvas ] = useCanvas()
+  if (!handleElement.value) return;
+  const [canvas] = useCanvas();
   if (openColorMask.value) {
-    const blendColorFilter = handleElement.value.filters.filter(obj => obj.type === BlendColorFilter)[0]
+    const blendColorFilter = handleElement.value.filters.filter(
+      (obj) => obj.type === BlendColorFilter
+    )[0];
     if (!blendColorFilter) {
       const blendFilter = new filters.BlendColor({
         color: maskColor.value,
-        mode: 'add',
-        alpha: maskAlpha.value
-      })
-      handleElement.value.filters.push(blendFilter as filters.BaseFilter)
-      handleElement.value.applyFilters()
+        mode: "add",
+        alpha: maskAlpha.value,
+      });
+      handleElement.value.filters.push(blendFilter as filters.BaseFilter);
+      handleElement.value.applyFilters();
     }
+  } else {
+    handleElement.value.filters = handleElement.value.filters.filter(
+      (obj) => obj.type !== BlendColorFilter
+    );
+    handleElement.value.applyFilters();
   }
-  else {
-    handleElement.value.filters = handleElement.value.filters.filter(obj => obj.type !== BlendColorFilter)
-    handleElement.value.applyFilters()
-  }
-  canvas.renderAll()
-}
-
-
+  canvas.renderAll();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -122,7 +147,7 @@ const toggleColorMask = () => {
   display: flex;
   align-items: center;
 }
-.slider-num{
+.slider-num {
   display: flex;
   align-items: center;
   justify-content: center;
--- src/views/Editor/CanvasRight/ElementStylePanel/ImageStylePanel.vue
+++ src/views/Editor/CanvasRight/ElementStylePanel/ImageStylePanel.vue
@@ -1,21 +1,54 @@
 <template>
   <div class="image-style-panel">
-    <ElementPosition/>
+    <ElementPosition />
     <el-divider />
-    
-    <div class="origin-image" :style="{ backgroundImage: `url(${handleElement.originSrc ? handleElement.originSrc : handleElement.getSrc()})` }"></div>
+
+    <div
+      class="origin-image"
+      :style="{
+        backgroundImage: `url(${
+          handleElement.originSrc
+            ? handleElement.originSrc
+            : handleElement.getSrc()
+        })`,
+      }"
+    ></div>
 
     <ElementFlip />
 
     <el-row class="mt-10">
-      <el-col :span="12">
-        <el-button-group class="clip-image">
-          <el-button class="clip-button" @click="clipImage">
-            <IconTailoring class="btn-icon" /> 裁剪
-          </el-button>
-          <el-popover trigger="click" width="284">
-            <template #reference>
-              <el-button><IconDown /></el-button>
+      <el-button-group class="clip-image">
+        <el-button class="clip-button" @click="clipImage">
+          <IconTailoring class="btn-icon" /> {{ $t("style.cropImage") }}
+        </el-button>
+        <el-popover trigger="click" width="284">
+          <template #reference>
+            <el-button><IconDown /></el-button>
+          </template>
+          <div class="clip">
+            <div class="title">{{ $t("style.byShape") }}：</div>
+            <div class="shape-clip">
+              <div
+                class="shape-clip-item"
+                v-for="(item, key) in CLIPPATHS"
+                :key="key"
+                @click="presetImageClip(key)"
+              >
+                <div class="shape" :style="{ clipPath: item.style }"></div>
+              </div>
+            </div>
+
+            <template v-for="type in ratioClipOptions" :key="type.label">
+              <div class="title" v-if="type.label">按{{ type.label }}：</div>
+              <el-button-group class="row">
+                <el-button
+                  style="flex: 1"
+                  v-for="item in type.children"
+                  :key="item.key"
+                  @click="presetImageClip('rect', item.ratio)"
+                  >{{ item.key }}</el-button
+                >
+              </el-button-group>
             </template>
             <div class="clip">
               <div class="title">按形状：</div>
@@ -61,57 +94,71 @@
     <el-divider />
     <ElementShadow :hasShadow="hasShadow" />
     <el-divider />
-    
+
     <el-row>
-      <FileInput class="full-width-btn" @change="(files: FileList) => replaceImage(files)" >
-        <el-button class="full-btn"><IconTransform class="btn-icon" /> 替换图片</el-button>
+      <FileInput
+        class="full-width-btn"
+        @change="(files: FileList) => replaceImage(files)"
+      >
+        <el-button class="full-btn"
+          ><IconTransform class="btn-icon" />
+          {{ $t("style.replaceimage") }}</el-button
+        >
       </FileInput>
     </el-row>
     <el-row>
-      <el-button class="full-width-btn" @click="resetImage()"><IconUndo class="btn-icon" /> 重置样式</el-button>
+      <el-button class="full-width-btn" @click="resetImage()"
+        ><IconUndo class="btn-icon" /> {{ $t("style.resetStyle") }}</el-button
+      >
     </el-row>
     <el-row>
-      <el-button class="full-width-btn" @click="setBackgroundImage()"><IconTheme class="btn-icon" /> 设为背景</el-button>
+      <el-button class="full-width-btn" @click="setBackgroundImage()"
+        ><IconTheme class="btn-icon" /> {{ $t("style.setAsBg") }}</el-button
+      >
     </el-row>
     <ImageMatting :visible="dialogVisible" :image="handleElement.originSrc ? handleElement.originSrc : handleElement.getSrc()" @close="closeMatting" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useMainStore, useTemplatesStore } from '@/store'
-import { CLIPPATHS, ClipPathType } from '@/configs/images'
-import { ImageElement } from '@/types/canvas'
-import { ratioClipOptions } from '@/configs/images'
-import { getImageDataURL } from '@/utils/image'
-import { propertiesToInclude } from '@/configs/canvas'
-import { Image } from 'fabric'
-import ElementPosition from '../Components/ElementPosition.vue'
-import ElementOutline from '../Components/ElementOutline.vue'
-import ElementShadow from '../Components/ElementShadow.vue'
-import ElementFlip from '../Components/ElementFlip.vue'
-import ElementFilter from '../Components/ElementFilter.vue'
-import ElementMask from '../Components/ElementMask.vue'
-import useCanvas from '@/views/Canvas/useCanvas'
+import { ref, computed } from "vue";
+import { storeToRefs } from "pinia";
+import { useMainStore, useTemplatesStore } from "@/store";
+import { CLIPPATHS, ClipPathType } from "@/configs/images";
+import { ImageElement } from "@/types/canvas";
+import { ratioClipOptions } from "@/configs/images";
+import { getImageDataURL } from "@/utils/image";
+import { propertiesToInclude } from "@/configs/canvas";
+import { Image } from "fabric";
+import ElementPosition from "../Components/ElementPosition.vue";
+import ElementOutline from "../Components/ElementOutline.vue";
+import ElementShadow from "../Components/ElementShadow.vue";
+import ElementFlip from "../Components/ElementFlip.vue";
+import ElementFilter from "../Components/ElementFilter.vue";
+import ElementMask from "../Components/ElementMask.vue";
+import useCanvas from "@/views/Canvas/useCanvas";
 
-const mainStore = useMainStore()
-const templatesStore = useTemplatesStore()
-const [ canvas ] = useCanvas()
-const { canvasObject } = storeToRefs(mainStore)
-const handleElement = computed(() => canvasObject.value as Image)
-const hasShadow = computed(() => handleElement.value.shadow ? true : false)
+const mainStore = useMainStore();
+const templatesStore = useTemplatesStore();
+const [canvas] = useCanvas();
+const { canvasObject } = storeToRefs(mainStore);
+const handleElement = computed(() => canvasObject.value as Image);
+const hasShadow = computed(() => (handleElement.value.shadow ? true : false));
 const dialogVisible = ref(false);
 // 打开自由裁剪
 const clipImage = () => {
-  if (!handleElement.value) return
-  handleElement.value.set({__isCropping: true, clipPath: undefined, cropPath: undefined})
-  canvas.renderAll()
-}
+  if (!handleElement.value) return;
+  handleElement.value.set({
+    __isCropping: true,
+    clipPath: undefined,
+    cropPath: undefined,
+  });
+  canvas.renderAll();
+};
 
 // 预设裁剪
 const presetImageClip = (key: ClipPathType, ratio = 0) => {
-  if (!handleElement.value) return
+  if (!handleElement.value) return;
   // const originImage = handleElement.value._originalElement
   // const originHeight = originImage.height
   // const originWidth = originImage.width
@@ -119,11 +166,9 @@ const presetImageClip = (key: ClipPathType, ratio = 0) => {
   // 纵横比裁剪（形状固定为矩形）
   if (ratio) {
     // const imageRatio = originHeight / originWidth
-
     // const min = 0
     // const max = 100
     // let range: [[number, number], [number, number]]
-
     // if (imageRatio > ratio) {
     //   const distance = ((1 - ratio / imageRatio) / 2) * 100
     //   range = [[min, distance], [max, max - distance]]
@@ -141,46 +186,44 @@ const presetImageClip = (key: ClipPathType, ratio = 0) => {
   // 形状裁剪（保持当前裁剪范围）
   else {
     // const path = CLIPPATHS[key].createPath(200, 200)
-    handleElement.value.set({__isCropping: true, _cropKey: key})
-    canvas.renderAll()
+    handleElement.value.set({ __isCropping: true, _cropKey: key });
+    canvas.renderAll();
   }
-}
+};
 
 const openMatting = () => {
-  dialogVisible.value = true
-}
+  dialogVisible.value = true;
+};
 
 const closeMatting = () => {
-  dialogVisible.value = false
-}
+  dialogVisible.value = false;
+};
 
 // 替换图片（保持当前的样式）
 const replaceImage = (files: FileList) => {
-  const imageFile = files[0]
-  if (!imageFile) return
-  getImageDataURL(imageFile).then(dataURL => {
-    const props = { src: dataURL }
-    handleElement.value.setSrc(dataURL)
-    templatesStore.updateElement({ id: handleElement.value.id, props })
-  })
-  
-}
+  const imageFile = files[0];
+  if (!imageFile) return;
+  getImageDataURL(imageFile).then((dataURL) => {
+    const props = { src: dataURL };
+    handleElement.value.setSrc(dataURL);
+    templatesStore.updateElement({ id: handleElement.value.id, props });
+  });
+};
 
 // 重置图片：清除全部样式
 const resetImage = () => {
-  handleElement.value.filters = []
-  handleElement.value.applyFilters()
+  handleElement.value.filters = [];
+  handleElement.value.applyFilters();
   // @ts-ignore
-  const props = handleElement.value.toObject(propertiesToInclude) as ImageElement
-  templatesStore.updateElement({ id: props.id, props})
-
-  
-}
+  const props = handleElement.value.toObject(
+    propertiesToInclude
+  ) as ImageElement;
+  templatesStore.updateElement({ id: props.id, props });
+};
 
 // 将图片设置为背景
 const setBackgroundImage = () => {
   // const _handleElement = handleElement.value as PPTImageElement
-
   // const background: SlideBackground = {
   //   ...currentSlide.value.background,
   //   type: 'image',
@@ -188,8 +231,8 @@ const setBackgroundImage = () => {
   //   imageSize: 'cover',
   // }
   // slidesStore.updateSlide({ background })
-  // 
-}
+  //
+};
 </script>
 
 <style lang="scss" scoped>
