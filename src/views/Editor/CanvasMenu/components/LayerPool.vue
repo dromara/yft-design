963222356d1b41d2fb73c082a45fa5ea7d3bb2b2
June <1601745371@qq.com>
2024-04-04 13:53:45 +0800

    fix: 修复图层的搜索图层无法键入

--- src/views/Editor/CanvasMenu/components/LayerPool.vue
+++ src/views/Editor/CanvasMenu/components/LayerPool.vue
@@ -1,9 +1,17 @@
+<!--
+ * @Author: June
+ * @Description: 搜索图层
+ * @Date: 2024-03-27 20:18:17
+ * @LastEditors: June
+ * @LastEditTime: 2024-04-04 13:52:26
+-->
 <template>
   <div @click.stop="cancelElement">
     <el-row class="layout-search">
       <el-input
+        v-model="keywords"
         :prefix-icon="Search"
-        :placeholder="t('message.SearchLayer')"
+        :placeholder="$t('message.searchLayer')"
       ></el-input>
     </el-row>
     <LayerDraggableSelf
@@ -14,7 +22,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref, unref } from "vue";
 import { Search } from "@element-plus/icons-vue";
 import { storeToRefs } from "pinia";
 import { useTemplatesStore } from "@/store";
@@ -22,19 +30,26 @@ import { ElementNames } from "@/types/elements";
 import { WorkSpaceThumbType } from "@/configs/canvas";
 import LayerDraggableSelf from "./LayerComponents/LayerDraggableSelf.vue";
 import useHandleElement from "@/hooks/useHandleElement";
-import useI18n from "@/hooks/useI18n";
+
+// 搜索关键字
+const keywords = ref('')
 
 const templatesStore = useTemplatesStore();
-const { t } = useI18n();
 const { currentTemplate } = storeToRefs(templatesStore);
 const { cancelElement } = useHandleElement();
-const layerObjects = computed(() =>
-  currentTemplate.value.objects.filter(
+const layerObjects = computed(() => {
+  const _keywords = unref(keywords)
+  if(!_keywords) return currentTemplate.value.objects.filter(
     (item) =>
       !WorkSpaceThumbType.includes(item.id) &&
       item.type.toLowerCase() !== ElementNames.REFERENCELINE
   )
-);
+  return currentTemplate.value.objects.filter(
+    (item) =>
+      !WorkSpaceThumbType.includes(item.id) &&
+      item.type.toLowerCase() !== ElementNames.REFERENCELINE && item.type.toLowerCase().includes(_keywords) 
+  )
+});
 </script>
 
 <style lang="scss" scoped>
