550506dea0e74765d9e687eca961043605dbf796
Nevermore <15972699417@163.com>
2024-04-23 17:43:04 +0800

    更新移动端操作

--- src/views/Canvas/useHammer.ts
+++ src/views/Canvas/useHammer.ts
@@ -11,8 +11,8 @@ export default () => {
   const initHammer = () => {
     let pausePanning = false
 
-    let lastX = null
-    let lastY = null
+    let lastX: number = 0
+    let lastY: number = 0
 
     let adjustDeltaX = 0
     let adjustDeltaY = 0
@@ -21,12 +21,12 @@ export default () => {
     let adjustScaleY = 1
     let adjustRotation = 0
 
-    let currentDeltaX = null
-    let currentDeltaY = null
-    let currentScale = null
-    let currentScaleX = null
-    let currentScaleY = null
-    let currentRotation = null
+    let currentDeltaX = 0
+    let currentDeltaY = 0
+    let currentScale = 0
+    let currentScaleX = 0
+    let currentScaleY = 0
+    let currentRotation = 0
 
     const [canvas] = useCanvas()
     const fabricStore = useFabricStore()
@@ -42,32 +42,29 @@ export default () => {
 
     hammer.on("panstart pinchstart rotatestart", (e) => {
       adjustRotation -= e.rotation
-      this.lastX = e.center.x
-      this.lastY = e.center.y
+      lastX = e.center.x
+      lastY = e.center.y
       if (canvas.getActiveObject()) {
         const object = canvas.getActiveObject()
         if (!object) return
-        this.adjustScaleX = object.scaleX
-        this.adjustScaleY = object.scaleY
+        adjustScaleX = object.scaleX
+        adjustScaleY = object.scaleY
       }
     })
 
     hammer.on("panmove", (e) => {
 
-      if (canvas.getActiveObject() == undefined
-        && this.pausePanning == false
-        && canvas.isDrawingMode == 0
-        && e.maxPointers == 1) {
+      if (canvas.getActiveObject() == undefined && pausePanning == false && !canvas.isDrawingMode && e.maxPointers == 1) {
 
-        currentDeltaX = -(this.lastX - e.center.x)
-        currentDeltaY = -(this.lastY - e.center.y)
-        let delta = new fabric.Point(currentDeltaX, currentDeltaY)
+        currentDeltaX = -(lastX - e.center.x)
+        currentDeltaY = -(lastY - e.center.y)
+        let delta = new Point(currentDeltaX, currentDeltaY)
 
         canvas.relativePan(delta)
         canvas.renderAll()
 
-        this.lastX = e.center.x
-        this.lastY = e.center.y
+        lastX = e.center.x
+        lastY = e.center.y
       }
     })
 
@@ -87,8 +84,10 @@ export default () => {
         ctx.translate(middlePoint.x, middlePoint.y);
         ctx.rotate((Math.PI / 180) * object.angle)
         ctx.translate(-middlePoint.x, -middlePoint.y);
-        ctx.strokeStyle = centerLineColor
-        ctx.lineWidth = centerLineWidth
+        // ctx.strokeStyle = centerLineColor
+        // ctx.lineWidth = centerLineWidth
+        ctx.strokeStyle = '#3988ad'
+        ctx.lineWidth = 1
         ctx.beginPath()
         ctx.moveTo(((XYstart.x)), ((XYstart.y)))
         ctx.lineTo(((XYend.x)), ((XYend.y)))
@@ -107,18 +106,8 @@ export default () => {
         ctx.strokeStyle = '#3988ad'
         ctx.lineWidth = 1
         ctx.beginPath()
-        //console.log("centerLineColor: " + centerLineColor)
-        ctx.moveTo(
-          ((XYstart.x)),
-          ((XYstart.y))
-        )
-
-        ctx.lineTo(
-          ((XYend.x)),
-          ((XYend.y))
-        )
-
-
+        ctx.moveTo( XYstart.x, XYstart.y)
+        ctx.lineTo(XYend.x, XYend.y)
         ctx.stroke()
         ctx.restore()
 
@@ -126,31 +115,28 @@ export default () => {
       }
     }
 
-    const checkRotateSnap = (degree, object) => {
+    const between = (value: number, first: number, last: number) => {
+      let lower = Math.min(first,last) , upper = Math.max(first,last);
+      return value >= lower &&  value <= upper ;
+    }
+    
+    /** 
+    const checkRotateSnap = (degree: number, object: FabricObject) => {
       var inDegree = degree + adjustRotation
       var newDegree = null
       if (object.lockRotation == true) {
         if (between(Math.abs(Math.ceil(inDegree)), Math.abs(object.lockedDegree) - 10, Math.abs(object.lockedDegree) + 10)) {
-
-
-
-          //console.log("111 - maintain snap")
           object.lockRotation = true
           newDegree = object.lockedDegree
-          //newDegree = degree
-
-          // break
-        } else {
-
-          //console.log("222 - exit snap")
+        } 
+        else {
           object.lockRotation = false
           object.lockedDegree = null
           newDegree = degree + adjustRotation
           contextLines.clearRect(0, 0, canvas.width, canvas.height);
-
         }
-
-      } else {
+      } 
+      else {
 
         if (rotateSnaps.includes(Math.abs(Math.ceil(inDegree)))) {
           object.lockRotation = true
@@ -166,11 +152,103 @@ export default () => {
           contextLines.clearRect(0, 0, canvas.width, canvas.height);
         }
       }
-      update_info()
+      // update_info()
       return newDegree
 
 
     }
+    **/
+
+    hammer.on("pinchmove rotatemove", (e) => {
+      console.log(e.scale)
+      canvas.zoomToPoint({ x:e.deltaX, y: e.deltaY }, e.scale);
+      return;
+      // if(canvas.getActiveObject() && e.maxPointers == 2) {
+  
+      //   this.pausePanning = true
+      //   var object = canvas.getActiveObject()
+
+      //   this.currentRotation = checkRotateSnap(e.rotation, object)
+          
+      //     //console.log("this.currentRotation: " + this.currentRotation)
+          
+      //   if (this.currentRotation != null) {
+      //     object.rotate(this.currentRotation)
+      //   }
+          
+  
+      //     // -----------------------------------
+      //     // SCALING
+      //     // -----------------------------------
+  
+      //     currentScale = adjustScale * e.scale
+      //     currentScaleX = adjustScaleX * e.scale * 1
+      //     currentScaleY = adjustScaleY * e.scale * 1
+  
+      //     // Blocks object from being resized too small (and maintains aspect ratio)
+      //     if (currentScaleX > object.minScaleLimit && currentScaleY > object.minScaleLimit) {
+  
+      //     } else {
+      //         // console.log("object has reached a limit")
+      //         currentScaleX = object.scaleX //Math.max(currentScaleX, object.minScaleLimit)
+      //         currentScaleY = object.scaleY //Math.max(currentScaleY, object.minScaleLimit)     
+      //     }
+  
+      //     this.pausePanning = true
+  
+      //     let deltaScaleX = currentScaleX
+      //     let deltaScaleY = currentScaleY
+  
+      //     // Temporarily locking regular object dragging during the gesture, just to smooth out jitteryness
+      //     object.set({ lockMovementX: true, lockMovementY: true })
+  
+      //     object.set('scaleX', deltaScaleX)
+      //     object.set('scaleY', deltaScaleY)
+      //     object.setCoords()
+          
+          
+      //     canvas.renderAll() 
+          
+          
+      //             if(object.lockRotation == true) {
+           
+      //         drawRotateGuidelines(object)           
+              
+      //     }
+          
+      //     object.setCoords()
+          
+          
+      //     canvas.renderAll() 
+  
+      // }
+    })
+  
+  
+  
+  hammer.on("panend pinchend rotateend", (e) => {
+    pausePanning = false
+    // contextLines.clearRect(0, 0, canvas.width, canvas.height);
+    const object = canvas.getActiveObject()
+    if (object) {
+      
+      adjustScale = currentScale;
+      adjustRotation = currentRotation;
+      adjustDeltaX = currentDeltaX;
+      adjustDeltaY = currentDeltaY;
+      adjustScaleX = currentScaleX;
+      adjustScaleY = currentScaleY;
+
+      setTimeout(function(){
+        object.set({
+          lockRotation: false,
+          lockMovementX: false,
+          lockMovementY: false
+        })
+      }, 300)
+    }
+    canvas.renderAll()
+  })
 
     canvas.on('object:rotating', function (e) {
       if (rotateSnaps.includes(Math.abs(Math.ceil(e.target.angle)))) {
