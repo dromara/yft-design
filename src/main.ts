32b69eb43a747b752a3fbd05fc10fc47483fea44
zhao_juchang <2233737325@qq.com>
2024-04-25 11:50:08 +0800

    style: 引入elementPlus css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import App from "./App.vue"
 import { setupI18n } from '@/plugins/i18n'
+import 'element-plus/dist/index.css'
 
 import "@/extension/index"
 
