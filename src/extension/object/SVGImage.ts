0c53d13acb6bd049c751070f435f85d5559bf3a3
Nevermore <15972699417@163.com>
2024-06-11 16:15:34 +0800

    更新矢量图片显示bug

--- src/extension/object/SVGImage.ts
+++ src/extension/object/SVGImage.ts
@@ -1,18 +1,27 @@
 import { getImageSize } from '@/utils/image'
-import { Image, Object as FabricObject, util, classRegistry, ImageProps, ImageSource } from 'fabric'
+import { Image, Object as FabricObject, util, classRegistry, ImageProps, ImageSource, SerializedImageProps, TClassProperties } from 'fabric'
 
 export class SVGImage extends Image {
   static type: string = 'svgimage'
-  originWidth?: number
-  originHeight?: number
+  public originWidth?: number
+  public originHeight?: number
+  public originScale?: number
+  public srcWidth?: number
+  public srcHeight?: number
 
   static async getScale (src: string, object: SVGImage) {
-    const { width, height } = await getImageSize(src)
-    object.originWidth = object.originWidth ? object.originWidth : object.width
-    object.originHeight = object.originHeight ? object.originHeight : object.height
-    const widthScale = object.originWidth / width
-    const heigthScale = object.originHeight / height
+    if (!object.srcWidth && !object.srcHeight) {
+      const { width, height } = await getImageSize(src)
+      object.srcWidth = width
+      object.srcHeight = height
+    }
+    if (!object.srcWidth || !object.srcHeight) return
+    const widthScale = object.width / object.srcWidth
+    const heigthScale = object.height / object.srcHeight
     const scale = widthScale > heigthScale ? widthScale : heigthScale
+    if (!object.originWidth) object.originWidth = object.width
+    if (!object.originHeight) object.originHeight = object.height
+    if (!object.originScale) object.originScale = object.scaleX
     object.scaleX = object.scaleY = scale
     object.width = object.originWidth / scale
     object.height = object.originHeight / scale
@@ -24,14 +33,13 @@ export class SVGImage extends Image {
 
   static async fromObject({ filters: f, resizeFilter: rf, src, crossOrigin, ...object }: any, options: { signal: AbortSignal }): Promise<Image> {
     await this.getScale(src, object)
-    console.log('width:', object.width, 'height:', object.height, 'originWidth:', object.originWidth, 'originHeight:', object.originHeight)
     return Promise.all([
       util.loadImage(src, { ...options, crossOrigin }),
       f && util.enlivenObjects(f, options),
       rf && util.enlivenObjects([rf], options),
       util.enlivenObjectEnlivables(object, options),
     ]).then(([el, filters = [], [resizeFilter] = [], hydratedProps = {}]) => {
-      return new this(el, {
+      const image = new this(el, {
         ...object,
         src,
         crossOrigin,
@@ -39,7 +47,23 @@ export class SVGImage extends Image {
         resizeFilter,
         ...hydratedProps,
       });
+      if (object.originWidth) image.originWidth = object.originWidth
+      if (object.originHeight) image.originHeight = object.originHeight
+      if (object.originScale) image.originScale = object.originScale
+      if (object.srcWidth) image.srcWidth = object.srcWidth
+      if (object.srcHeight) image.srcHeight = object.srcHeight
+      return image
     });
+  }
+
+  toObject<T extends Omit<Partial<ImageProps> & TClassProperties<this>, keyof SerializedImageProps>, K extends keyof T = never>(propertiesToInclude?: K[] | undefined): Pick<T, K> & SerializedImageProps {
+    const object = super.toObject(propertiesToInclude as any[]) as any
+    if (object.originWidth) object.width = object.originWidth
+    if (object.originHeight) object.height = object.originHeight
+    if (this.srcWidth) object.srcWidth = this.srcWidth
+    if (this.srcHeight) object.srcHeight = this.srcWidth
+    if (object.originScale) object.scaleX = object.scaleY = object.originScale
+    return object
   }
 }
 
